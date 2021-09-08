79dbb0c91db53148df2a8f72066e4c06ad69bcf5
Piyoshila <it20008932@my.sliit.lk>
2021-09-08 15:40:40 +0530

    Student CRUD API

--- aspire-backend/api/src/main/java/com/lms/api/controller/StudentController.java
+++ aspire-backend/api/src/main/java/com/lms/api/controller/StudentController.java
@@ -16,45 +16,63 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lms.api.model.Student;
-import com.lms.api.service.StudentService;
+import com.lms.api.repository.StudentRepository;
+import com.lms.api.exception.StudentNotFoundException;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class StudentController {
 
-	@Autowired
-	private final StudentService studentService;
+	private final StudentRepository SRepo;
 
-	public StudentController(StudentService studentService) {
-		this.studentService = studentService;
-	}
-
-	// API to get student by ID
-	@GetMapping("/student/{id}")
-	public ResponseEntity<Student> get(@PathVariable String id) {
-		return studentService.get(id)
-				.map(ResponseEntity::ok)
-				.orElse(ResponseEntity.notFound()
-						.build());
+	StudentController (StudentRepository repository) {
+		this.SRepo = repository;
 	}
 
 	// API to get all students
 	@GetMapping("/students")
 	public List<Student> list() {
-		return studentService.list();
+		return SRepo.findAll();
+	}
+
+	// API to get student by ID
+	@GetMapping("/student/{id}")
+	public Student get(@PathVariable String id) {
+		return SRepo.findById(id)
+				.orElseThrow(() -> new StudentNotFoundException(id));
 	}
 
 	// API to create a student
-	@PostMapping("/addstudent")
-	public String save(@RequestBody Student student) {
-		return studentService.save(student)
-				.getId();
+	@PostMapping("/addStudent")
+	Student create(@RequestBody Student newStudent) {
+		return SRepo.save(newStudent);
+	}
+
+	// API to update a student
+	@PutMapping("/update/{id}")
+	public Student update(@PathVariable String id, @RequestBody Student student) {
+		return SRepo.findById(id)
+				.map(s -> {
+					s.setPid(student.getPid());
+					s.setFname(student.getFname());
+					s.setLname(student.getLname());
+					s.setDob(student.getDob());
+					s.setGender(student.getGender());
+					s.setPhone(student.getPhone());
+					s.setEmail(student.getEmail());
+					s.setPassword(student.getPassword());
+					return SRepo.save(s);
+				})
+				.orElseGet(() -> {
+					student.setId(id);
+					return SRepo.save(student);
+				});
 	}
 
 	// API to delete a student
 	@DeleteMapping("/delete/{id}")
-	public ResponseEntity<Map<String, Boolean>> delete(@PathVariable String id) {
-		return studentService.delete(id);
+	public void delete(@PathVariable String id) {
+		SRepo.deleteById(id);
 	}
 }
--- aspire-backend/api/src/main/java/com/lms/api/model/Student.java
+++ aspire-backend/api/src/main/java/com/lms/api/model/Student.java
@@ -8,23 +8,31 @@ public class Student {
 	
 	@Id
 	private String id;
-	private String sid;
+	private String pid;
 	private String fname;
 	private String lname;
+	private String dob;
+	private String gender;
+	private Number phone;
 	private String email;
+	private String password;
 	
 	public Student() {}
 	
 	public String getId() {
 		return id;
 	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
 	
-	public String getSid() {
-		return sid;
+	public String getPid() {
+		return pid;
 	}
 
-	public void setSid(String id) {
-		this.sid = id;
+	public void setPid(String id) {
+		this.pid = id;
 	}
 
 	public String getFname() {
@@ -35,20 +43,28 @@ public class Student {
 		this.fname = fname;
 	}
 
-	public String getLname() {
-		return lname;
-	}
+	public String getLname() { return lname; }
 
-	public void setLname(String lname) {
-		this.lname = lname;
-	}
+	public void setLname(String lname) { this.lname = lname; }
 
-	public String getEmail() {
-		return email;
-	}
+	public String getDob() { return dob; }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
+	public void setDob(String dob) { this.dob = dob; }
+
+	public String getGender() { return gender; }
+
+	public void setGender(String gender) { this.gender = gender; }
+
+	public Number getPhone() { return phone; }
+
+	public void setPhone(Number phone) { this.phone = phone; }
+
+	public String getEmail() { return email; }
+
+	public void setEmail(String email) { this.email = email; }
+
+	public String getPassword() { return password; }
+
+	public void setPassword(String password) { this.password = password; }
+
 }
