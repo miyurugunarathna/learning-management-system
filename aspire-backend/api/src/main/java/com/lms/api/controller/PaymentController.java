be0b13a577841e746f7f54d70ef7a1c21e65d7c9
miyurugunarathna <33316556+miyurugunarathna@users.noreply.github.com>
2021-09-17 03:55:03 +0530

    Fees and Payment backend

--- aspire-backend/api/src/main/java/com/lms/api/controller/PaymentController.java
+++ aspire-backend/api/src/main/java/com/lms/api/controller/PaymentController.java
@@ -3,37 +3,31 @@ package com.lms.api.controller;
 import com.lms.api.model.Payment;
 import com.lms.api.repository.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.aggregation.Aggregation;
-import org.springframework.data.mongodb.core.aggregation.AggregationResults;
-import org.springframework.data.mongodb.core.aggregation.LookupOperation;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class PaymentController {
     @Autowired
     private PaymentRepository paymentRepository;
-    @Autowired
-    private MongoTemplate mongoTemplate;
 
     @PostMapping("/payments/add")
     public String savePayment(@RequestBody Payment payment) {
         paymentRepository.save(payment);
-        return "Added payment with id: " + payment.getPaymentID();
+        return String.valueOf(payment.getPaymentID());
     }
 
     @GetMapping("/payments")
     public List<Payment> getPayments() {
-        LookupOperation lookupOperation = LookupOperation.newLookup().
-                from("fee").
-                localField("feeID").
-                foreignField("_id").
-                as("fees");
-        Aggregation aggregation = Aggregation.newAggregation(lookupOperation);
-        AggregationResults<Payment> results = mongoTemplate.aggregate(aggregation, "payment", Payment.class);
-        return results.getMappedResults();
+        return paymentRepository.findAll();
+    }
+
+    @GetMapping("/payments/{id}")
+    public Optional<Payment> getFee(@PathVariable String id) {
+        return paymentRepository.findById(id);
     }
 }
